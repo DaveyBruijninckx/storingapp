f234159ff5e0139a7b86c087ed1c09a235106e2f
DenDave69 <daveydenbasta@gmail.com>
2026-04-10 11:39:34 +0200

    inlog verbetert

---
+++ resources/views/login/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title>StoringApp</title>
+    <?php require_once '../components/head.php'; ?>
+</head>
+
+<body>
+
+    <?php require_once '../components/header.php'; ?>
+
+    <div class="container home">
+        <form action="../../../app/Http/Controllers" method="POST">
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <input type="submit" value="login">
+        </form>
+    </div>
+
+</body>
+
+</html>
